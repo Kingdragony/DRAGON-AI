60133631a984fe689f98b579c1515b38fa592546
𝕬𝖗𝖑𝖔𝖉𝖗𝖆𝖌𝖔𝖓. <161493578+Kingdragony@users.noreply.github.com>
2025-05-27 12:41:54 +0300

    👾ᴅʀᴀɢᴏɴ ᴀɪ👾

--- action/index.js
+++ action/index.js
@@ -92,7 +92,7 @@ async function startRaven() {
             
 if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
       let m = smsg(client, mek, store);
-      const raven = require("../arlodragon");
+      const raven = require("../action/arlodragon");
       raven(client, m, chatUpdate, store);
     } catch (err) {
       console.log(err);
